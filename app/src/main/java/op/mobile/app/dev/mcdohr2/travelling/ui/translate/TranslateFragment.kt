c32f51516461fea2c2693135064548cd3524e8d2
Hayden-McD <mcdohr2@student.op.ac.nz>
2022-11-03 00:15:18 +1300

    Added an image button for text to speech, added spinner for future implimentation of language selection for translation

--- app/src/main/java/op/mobile/app/dev/mcdohr2/travelling/ui/translate/TranslateFragment.kt
+++ app/src/main/java/op/mobile/app/dev/mcdohr2/travelling/ui/translate/TranslateFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 import op.mobile.app.dev.mcdohr2.travelling.R
 
@@ -14,8 +15,18 @@ class TranslateFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_translate, container, false)
-
-
+//        val languages = resources.getStringArray(R.array.LANGUAGES)
+//
+//        val spinner: Spinner = view.findViewById(R.id.spinner_languages)
+//
+//        if (spinner != null) {
+//            val adapter = ArrayAdapter(
+//                this,
+//                android.R.layout.simple_spinner_item, languages
+//            )
+//            spinner.adapter = adapter
+//        }
         return view
     }
+
 }
