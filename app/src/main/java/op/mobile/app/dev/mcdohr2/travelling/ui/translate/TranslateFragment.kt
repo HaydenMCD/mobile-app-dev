8773a79bb68b4f02b3ae9e6c2e3acb1a39e1c6fd
Hayden-McD <mcdohr2@student.op.ac.nz>
2022-11-03 01:20:23 +1300

    Text to speech is working with english

--- app/src/main/java/op/mobile/app/dev/mcdohr2/travelling/ui/translate/TranslateFragment.kt
+++ app/src/main/java/op/mobile/app/dev/mcdohr2/travelling/ui/translate/TranslateFragment.kt
@@ -1,20 +1,39 @@
 package op.mobile.app.dev.mcdohr2.travelling.ui.translate
 
+import android.content.Context
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 import op.mobile.app.dev.mcdohr2.travelling.R
+import java.util.*
 
 class TranslateFragment: Fragment() {
+    lateinit var tts: TextToSpeech
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val view = inflater.inflate(R.layout.fragment_translate, container, false)
+
+        val btnTTS: ImageButton = view.findViewById(R.id.ibtn_text_to_speech)
+        val etText: EditText = view.findViewById(R.id.et_translation_text)
+
+        btnTTS.setOnClickListener{
+            tts = TextToSpeech(context, TextToSpeech.OnInitListener {
+                if(it == TextToSpeech.SUCCESS){
+                    tts.language = Locale.US
+                    tts.setSpeechRate(1.0f)
+                    tts.speak(etText.text.toString(), TextToSpeech.QUEUE_ADD, null)
+                }
+            })
+        }
+
 //        val languages = resources.getStringArray(R.array.LANGUAGES)
 //
 //        val spinner: Spinner = view.findViewById(R.id.spinner_languages)
@@ -26,6 +45,8 @@ class TranslateFragment: Fragment() {
 //            )
 //            spinner.adapter = adapter
 //        }
+
+
         return view
     }
 
